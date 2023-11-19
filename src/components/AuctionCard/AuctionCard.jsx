72fdb361aa28b47080897a5f7bad450cc4dd8066
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-11-18 18:07:50 -0600

    applied changes

--- src/components/AuctionCard/AuctionCard.jsx
+++ src/components/AuctionCard/AuctionCard.jsx
@@ -3,10 +3,11 @@ import { Container, Paper, Button, Chip, Avatar } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import "./AuctionCard.css";
 import { useNavigate } from "react-router-dom";
+import { LinearProgressWithLabel } from '../ProgressBar/ProgressBar';
 
 export const AuctionCard = ({auctionInfo}) => {
   const Item = styled(Paper)(({ theme }) => ({
-      backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+      backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#abebbc",
       ...theme.typography.body2,
       textAlign: "left",
       padding: "1rem",
@@ -19,28 +20,33 @@ export const AuctionCard = ({auctionInfo}) => {
     navigate("/auction_page", {state: data});
   };
 
+  const progress = auctionInfo.Progress / auctionInfo.Goal * 100;
+
   return (
-    <Item onClick={() => handleClick(auctionInfo)} className="post-item">
-      <div className="post-header">
-        <h2 className="post-top">
-          <div className="post-title"> {auctionInfo.Name} </div>
-        </h2>
-        <h3>
-            {auctionInfo.Title}
-        </h3>
-        <div className="post-user">
+    <Item className="post-item">
+      <div className='left-div'>
+        <h2 className="post-title"> {auctionInfo.Title} </h2>
+        <div className='org-div'>
           <Avatar
-            className="profile-pic"
-            sx={{ width: 25, height: 25, marginBottom: ".5rem" }}
-            src={auctionInfo.Logo ? auctionInfo.Logo : ""}
-          ></Avatar>
+              className="profile-pic"
+              sx={{ width: 25, height: 25, marginBottom: ".5rem" }}
+              src={auctionInfo.Logo ? auctionInfo.Logo : ""}
+            ></Avatar>
+          <h3> {auctionInfo.Name} </h3>
         </div>
+        <p className='post-text'>Starts: {auctionInfo.StartDate} </p>
+        <p className='post-text'>Ends: {auctionInfo.EndDate} </p>
+        <div>
+          <LinearProgressWithLabel value={progress}/>
+          <p className='post-text'> ${auctionInfo.Progress} / ${auctionInfo.Goal}</p>
+        </div>
+        <p className='post-text'>Active Auctions: {auctionInfo.ActiveAuctions}</p>
       </div>
-      <p className="post-text">{auctionInfo.Description}</p>
-      
-      <p className="post-location">
-        <b>Active Auctions</b>: {auctionInfo.ActiveAuctions}
-      </p>
+      <div className='right-div'>
+        <Button className='enter-button' variant="contained" onClick={() => handleClick(auctionInfo)}>
+          Enter
+        </Button>
+      </div>
     </Item>
   )
 }
---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export function LinearProgressWithLabel(props) {
+    return (
+        <Box sx={{width: '100%'}}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ mr: 2 }}>
+                    <Typography sx={{ ml: 0, mt: 0.5 }} variant="body1" color="black">Progress</Typography>
+                </Box>
+                <Box sx={{ width: '100%', mr: 1, mt: 0.5 }}>
+                    <Box
+                        sx={{
+                            width: '100%',
+                            height: "1rem",
+                            mr: 1,
+                            bgcolor: 'rgb(255, 255, 255)',
+                            verticalAlign: "middle",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <LinearProgress sx={{
+                            backgroundColor: 'white',
+                            '& .MuiLinearProgress-bar': {
+                                backgroundColor: 'green'
+                            },
+                            ml: 0.4,
+                            mr: 1,
+                            height: "0.6rem"
+                        }}
+                            variant="determinate" {...props} />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
+
+LinearProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate and buffer variants.
+     * Value between 0 and 100.
+     */
+    value: PropTypes.number.isRequired,
+};
