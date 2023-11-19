c43ce5ad79d7d1d37eb426d90364f3858d8b38a6
Zain Jerath <zainjerath@gmail.com>
2023-11-19 17:19:46 -0600

    homepage and basic styling

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,16 @@
 import './ProfilePage.css'
 import Banner from '../Banner/Banner';
-import { Container } from '@mui/material';
+import { Container, Avatar, Stack } from '@mui/material';
 
 const ProfilePage = () => {
   return (
-    <Container>
-        Profile
-        <Banner currentPage={"profile"} />
+    <Container maxWidth="sm">
+      <Stack className='profilestack'>
+        <Avatar style={{height: '3rem', width: '3rem'}}/>
+        <h2>First Last</h2>
+        <div>Rest of Profile Info</div>
+      </Stack>
+      <Banner currentPage={"profile"} />
     </Container>
   );
 };
