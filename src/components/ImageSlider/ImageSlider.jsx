6de9fc0a703233f40f9a524ccac2344e67bdb31f
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-11-19 19:37:47 -0600

    added images

---
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import './ImageSlider.css';
+const ImageSlider = ({ imagePath, onLeftClick, onRightClick, arrows }) => {
+
+    return (
+        <div className='slider-container'>
+            <div className='image-div'>
+                <img src={imagePath} alt="auction item" className='image' />
+            </div>
+            {arrows && <div className='arrows-div'>
+                <ChevronLeftIcon className='left-arrow' onClick={onLeftClick} />
+                <ChevronRightIcon className='right-arrow' onClick={onRightClick} />
+            </div>}
+        </div>
+
+    )
+}
+
+export default ImageSlider
