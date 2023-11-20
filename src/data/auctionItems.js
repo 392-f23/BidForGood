1a780f8fa46ed5d3485cece843136e346e328d01
murirop <panashemuriro2023@u.northwestern.edu>
2023-11-19 20:42:21 -0600

    add random data for auction items

---
+++ src/data/auctionItems.js
@@ -0,0 +1,40 @@
+const auctionItemData = {
+    0: [
+        {title: "Cozy Pet Bed", currentBid: "$30", numberOfBids: 8},
+        {title: "Paw Print Artwork", currentBid: "$20", numberOfBids: 5},
+        {title: "Interactive Cat Toy Set", currentBid: "$15", numberOfBids: 10},
+        {title: "Dog Grooming Voucher", currentBid: "$40", numberOfBids: 12},
+        {title: "Handcrafted Birdhouse", currentBid: "$25", numberOfBids: 6},
+        {title: "Pet Spa Day Package", currentBid: "$50", numberOfBids: 15},
+        {title: "Custom Pet Portrait Session", currentBid: "$35", numberOfBids: 9},
+        {title: "Animal-themed Blanket", currentBid: "$18", numberOfBids: 7},
+        {title: "Pet Photography Session", currentBid: "$55", numberOfBids: 11},
+        {title: "Cat Condo Tower", currentBid: "$45", numberOfBids: 14},
+    ],
+    1: [
+        { title: "Pet Bed and Toy Bundle", currentBid: "$20", numberOfBids: 8 },
+        { title: "Premium Dog Food Supply", currentBid: "$30", numberOfBids: 12 },
+        { title: "Cat Tree with Scratching Post", currentBid: "$25", numberOfBids: 10 },
+        { title: "Custom Pet Portrait Session", currentBid: "$40", numberOfBids: 15 },
+        { title: "Aquarium Starter Kit", currentBid: "$35", numberOfBids: 11 },
+        { title: "Pet Grooming Voucher", currentBid: "$18", numberOfBids: 7 },
+        { title: "Bird Watching Kit", currentBid: "$22", numberOfBids: 9 },
+        { title: "Reptile Habitat Set", currentBid: "$28", numberOfBids: 14 },
+        { title: "Horseback Riding Experience", currentBid: "$50", numberOfBids: 20 },
+        { title: "Exotic Fish Collection", currentBid: "$45", numberOfBids: 18 },
+    ], 
+    2: [
+        { title: "Children's Book Set", currentBid: "$25", numberOfBids: 7 },
+        { title: "Toy Train Playset", currentBid: "$20", numberOfBids: 5 },
+        { title: "Interactive Learning Tablet", currentBid: "$30", numberOfBids: 10 },
+        { title: "Art Supplies Kit", currentBid: "$15", numberOfBids: 8 },
+        { title: "Outdoor Adventure Backpack", currentBid: "$35", numberOfBids: 12 },
+        { title: "Superhero Costume Set", currentBid: "$28", numberOfBids: 9 },
+        { title: "Board Game Bundle", currentBid: "$22", numberOfBids: 6 },
+        { title: "Musical Instrument Kit", currentBid: "$40", numberOfBids: 15 },
+        { title: "Kids' Movie Night Package", currentBid: "$18", numberOfBids: 11 },
+        { title: "LEGO Building Blocks Set", currentBid: "$32", numberOfBids: 14 },
+    ]
+}
+  
+
