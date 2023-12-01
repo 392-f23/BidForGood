888d2eaa7bf5d4ebb99a0ab498bcc7a7d0dee241
Zain Jerath <zainjerath@gmail.com>
2023-11-30 21:46:15 -0600

    org side done

--- src/components/OrganizationHome/PastAuctionsPage.jsx
+++ src/components/OrganizationHome/PastAuctionsPage.jsx
@@ -13,12 +13,12 @@ export const PastAuctionsPage = () => {
     AuctionName: "Annual Fall Auction Drive",
     ClosedAuctions: 12,
     Description: "PuppiesRUs's annual fall event benefiting golden retrievers",
-    EndDate: "12/20/23",
+    EndDate: "10/01/23",
     Goal: 5000,
     OrganizationName: "PuppiesRUs",
     PhoneNumber: "(847) 492-0990",
     StartDate: "08/20/23",
-    TotalRaised: 4683,
+    TotalRaised: 5000,
     Logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYVYubg0FIFviSxRsZmhWUNqM7ZDAv0PyAL8NOH9cHpg&s"
   };
   const images = ['https://i.pinimg.com/736x/83/43/1f/83431fc3313ce0e7ae20837e2fafad57.jpg','https://i0.wp.com/www.puppies-r-us.com/wp-content/uploads/2023/10/image5-11.png?fit=640%2C480&ssl=1','https://goodsstores.com/cdn/shop/products/8303471_A.eps_High_800x.jpg?v=1679338906'];
